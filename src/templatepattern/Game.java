0d0c37ec288ddd61909f6972c717ccc7c466bbd1
Abid <ahmedabid3409@gmail.com>
2022-06-15 00:55:23 +0600

    Nothing

--- src/templatepattern/Game.java
+++ src/templatepattern/Game.java
@@ -1,7 +1,7 @@
 package templatepattern;
 
 public abstract class Game {
-    abstract void initialize();
+    abstract void initialize();//Strategy,Factory,Template,Composition, Chain of responsibility, Singleton
     abstract void mid();
     abstract void end();
     public final void play()
